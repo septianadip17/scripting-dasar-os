5570cd63016e7cf5fecf58a34e10128c73642594
Septian Adi Pratama <septianadip7998@gmail.com>
2025-11-12 15:15:25 +0700

    adding new variable

--- jumlah_ganjil.sh
+++ jumlah_ganjil.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-
-# Data
 data=(3 6 7 8 9 11 4 9 5 2 2 4 6 6)
 
-# Variabel penghitung jumlah bilangan ganjil
-jumlah_ganjil=0
+sum_ganjil=0
+count_ganjil=0
 
 echo "Nilai ganjil:"
 
-# Loop untuk cek setiap angka
 for angka in "${data[@]}"; do
   if (( angka % 2 != 0 )); then
     echo -n "$angka "
-    (( jumlah_ganjil++ ))
+    (( sum_ganjil += angka ))
+    (( count_ganjil++ ))
   fi
 done
 
 echo
-echo "Total jumlah nilai ganjil: $jumlah_ganjil"
+echo "Total jumlah nilai ganjil: $count_ganjil"
+echo "Jumlah semua nilai ganjil (penjumlahan): $sum_ganjil"
